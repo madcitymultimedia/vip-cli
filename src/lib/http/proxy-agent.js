7abdc8c61d6db971fb97fe757d078eec5c41178c
Mohammad Jangda <batmoo@gmail.com>
2022-03-15 00:24:29 -0400

    Add support for SOCKS_PROXY & HTTP_PROXY
    Have them automatically use the Socks or HTTPS agent, when set.

---
+++ src/lib/http/proxy-agent.js
@@ -0,0 +1,30 @@
+/**
+ * External dependencies
+ */
+import SocksProxyAgent from 'socks-proxy-agent';
+import HttpsProxyAgent from 'https-proxy-agent';
+import debugLib from 'debug';
+
+/**
+ * Internal dependencies
+ */
+
+const debug = debugLib( '@automattic/vip:lib:http:proxy-agent' );
+
+export default function createProxyAgent() {
+	const socksProxy = process.env.VIP_PROXY || process.env.SOCKS_PROXY;
+	if ( socksProxy ) {
+		debug( `Enabling SOCKS proxy support using config: ${ socksProxy }` );
+
+		return new SocksProxyAgent( socksProxy );
+	}
+
+	const httpProxy = process.env.HTTP_PROXY;
+	if ( httpProxy ) {
+		debug( `Enabling HTTPS proxy support using config: ${ httpProxy }` );
+
+		return new HttpsProxyAgent( httpProxy );
+	}
+
+	return null;
+}
